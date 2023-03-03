30cb283731c3d8411b9230a88b7055a379ed410b
melissa.gillen <90412072+StamperM@users.noreply.github.com>
2023-03-02 21:46:10 -0500

    creating the base of th components

---
+++ my-app/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function home(){
+    return(
+        <div>
+            <h1>About Me</h1>
+            <p>
+               things about me blah blah blah 
+            </p>
+        </div>
+    )
+}
