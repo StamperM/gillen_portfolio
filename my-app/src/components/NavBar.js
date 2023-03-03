4384946c138d70ef71f41abb359e65bc01f4e6f1
melissa.gillen <90412072+StamperM@users.noreply.github.com>
2023-03-02 23:23:49 -0500

    updates to app.js

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -2,6 +2,27 @@ import React from "react";
 
 export default function NavBar(){
     return(
+      <ul>
+        <li className="nav">
+            <a href="#home" onClick={()=>handlePageChange('Home') } 
+            className={currentPage === 'Home'? 'active' : 'notActive'}>Home</a>
         
-    )
+        </li>
+        <li className="nav">
+            <a href="#about" onClick={()=>handlePageChange('About') } 
+            className={currentPage === 'About'? 'active' : 'notActive'}>About</a>
+        
+        </li>
+        <li className="nav">
+            <a href="resume" onClick={()=>handlePageChange('Resume') } 
+            className={currentPage === 'Resume'? 'active' : 'notActive'}>Resume</a>
+        
+        </li>
+        <li className="nav">
+            <a href="Contact" onClick={()=>handlePageChange('Contact') } 
+            className={currentPage === 'Contact'? 'active' : 'notActive'}>Contact</a>
+        
+        </li>
+      </ul>  
+    );
 }
