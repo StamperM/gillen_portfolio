abf1c12dc5b7285f1d7b7eafdb51d195beedb297
melissa.gillen <90412072+StamperM@users.noreply.github.com>
2023-03-05 23:36:10 -0500

    update portfolio

--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -3,10 +3,10 @@ import '../styles/Portfolio.css'
 export default function  Portfolio(){
     return(
 <div className='container'>
-<div className="row">
+<div className="row row-col">
   
   <div className="col card">
-    <img className="card-img-top" src='./newHeights2.png' alt='workoutsite' />
+    <img className="card-img-top pimg" src='./newHeights2.png' alt='workoutsite' />
     <div className="card-body">
       <h5 className="card-title">New Heights Landing Page</h5>
       <p className="card-text"> <a href='https://stamperm.github.io/landing-page/' >Deployed Site</a></p>
@@ -14,7 +14,7 @@ export default function  Portfolio(){
     </div>
   </div>
   <div className="col card">
-    <img className="card-img-top" src='./poke.png' alt='pokedex' />
+    <img className="card-img-top pimg" src='./poke.png' alt='pokedex' />
     <div className="card-body">
       <h5 className="card-title">PokeDex</h5>
       <p className="card-text"> <a href='https://stamperm.github.io/velocirapt-vars--pokedex/' >Deployed Site</a></p>
@@ -22,19 +22,19 @@ export default function  Portfolio(){
     </div>
   </div>
   <div className="col card">
-    <img className="card-img-top"  src='./coming-soon.jpg' alt='coming soon' />
+    <img className="card-img-top pimg"  src='./coming-soon.jpg' alt='coming soon' />
     <div className="card-body">
       <h5 className="card-title">Coming Soon</h5>
-      <p className="card-text">T <a href='https://stamperm.github.io/walkItOut/'>Deployed Site</a></p>
+      <p className="card-text"> <a href='https://stamperm.github.io/walkItOut/'>Deployed Site</a></p>
       <p className="card-text"><small className="text-muted"><a href='https://github.com/StamperM/walkItOut' >GitHub Repo</a></small></p>
     </div>
 </div>
 
  </div>
  <div className='container'> 
-
+ <div className="row row-col">
   <div className="col card">
-    <img className="card-img-top" src='./weatherApp.png' alt='weathersite' />
+    <img className="card-img-top pimg" src='./weatherApp.png' alt='weathersite' />
     <div className="card-body">
       <h5 className="card-title">Weather App</h5>
       <p className="card-text"><a href='https://stamperm.github.io/WhatsTheWeather/' >Deployed Site</a></p>
@@ -42,7 +42,7 @@ export default function  Portfolio(){
     </div>
   </div>
   <div className="col card">
-    <img className="card-img-top" src='./workoutsite.png' alt='workoutsite' />
+    <img className="card-img-top pimg" src='./workoutsite.png' alt='workoutsite' />
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text"> <a href='https://homefitwellness.herokuapp.com/' >Deployed Site</a></p>
@@ -50,12 +50,13 @@ export default function  Portfolio(){
     </div>
   </div>
   <div className="col card">
-    <imgc className="card-img-top"  src='./OldPortfolio' alt='portfolio' />
+    <imgc className="card-img-top pimg"  src='./OldPortfolio.png' alt='portfolio' />
     <div className="card-body">
       <h5 className="card-title">Card title</h5>
       <p className="card-text"><a href='https://stamperm.github.io/gillen-portfolio-/' >Deployed Site</a></p>
       <p className="card-text"><small className="text-muted"><a href='https://github.com/StamperM/gillen-portfolio-' >GitHub Repo</a> </small></p>
   
+    </div>
     </div>
     </div>
     </div>
