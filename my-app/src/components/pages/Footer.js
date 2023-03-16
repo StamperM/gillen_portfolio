f0470c6460440e76e47f3fb589141ce769aa030a
melissa.gillen <90412072+StamperM@users.noreply.github.com>
2023-03-16 07:10:59 -0400

    added project 3

--- my-app/src/components/pages/Footer.js
+++ my-app/src/components/pages/Footer.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "../styles/Footer.css"
+import {Link} from "react-router-dom"
+import Li from "../../images/link.png"
+import GH from '../../images/github.svg'
 
 export default function Footer() {
   return (
     <div className="fixed-bottom  container cust-footer">
       <div className="row footer-ow">
         <div className="col">
-        <a
+        <Link
           title="linkedIn"
-          href="https://www.linkedin.com/in/melissa-gillen-34995a67/?trk=public-profile-join-page"
+          to ="https://www.linkedin.com/in/melissa-gillen-34995a67/?trk=public-profile-join-page"
         >
-         <img src='./LI-In-Bug.png' alt="linkedIn"/>
-        </a>
+         <img src={Li} alt="linkedIn"/>
+        </Link>
         </div>
     <div className="col">
-    <a title='githubAccount' href='https://github.com/StamperM'>
-            <img src="./github-mark-white.svg" alt="github"/> 
-</a>
+    <Link title='githubAccount' to='https://github.com/StamperM'>
+            <img src={GH} alt="github"/> 
+</Link>
     </div>
 
       </div>
--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import '../styles/Portfolio.css'
+import {Link} from 'react-router-dom'
+import Weather from '../../images/Weather.png'
+import Soccer from '../../images/Soccer.png'
+import Caffiends from '../../images/caffiends.png'
+import poke from '../../images/poke.png'
+import Old from '../../images/OldPortfolio.png'
+import Workout from '../../images/workoutsite.png'
+
+
+
+
+
 export default function  Portfolio(){
     return(
 <div className='container'>
 <div className="row row-col">
   
   <div className="col card cust-card">
-    <img className="card-img-top pimg" src='./newHeights2.png' alt='workoutsite' />
+    <img className="card-img-top pimg" src={Soccer} alt='workoutsite' />
     <div className="card-body">
       <h5 className="card-title">New Heights Landing Page</h5>
-      <p className="cust-card-text"> <a href='https://stamperm.github.io/landing-page/' >Deployed Site</a></p>
-      <p className="cust-card-text"><small className="text-muted"><a href='https://github.com/StamperM/landing-page' >GitHub Repo</a></small></p>
+      <p className="cust-card-text"> <Link to='https://stamperm.github.io/landing-page/' >Deployed Site</Link></p>
+      <p className="cust-card-text"><small className="text-muted"><Link to='https://github.com/StamperM/landing-page' >GitHub Repo</Link></small></p>
     </div>
   </div>
   <div className="col card cust-card">
-    <img className="card-img-top pimg" src='./poke.png' alt='pokedex' />
+    <img className="card-img-top pimg" src={poke} alt='pokedex' />
     <div className="card-body">
       <h5 className="card-title">PokeDex</h5>
-      <p className="cust-card-text"> <a href='https://stamperm.github.io/velocirapt-vars--pokedex/' >Deployed Site</a></p>
-      <p className="cust-card-text"><small className="text-muted"><a href='https://github.com/StamperM/velocirapt-vars--pokedex' >GitHub Repo</a></small></p>
+      <p className="cust-card-text"> <Link to='https://stamperm.github.io/velocirapt-vars--pokedex/' >Deployed Site</Link></p>
+      <p className="cust-card-text"><small className="text-muted"><Link to='https://github.com/StamperM/velocirapt-vars--pokedex' >GitHub Repo</Link></small></p>
     </div>
   </div>
   <div className="col card cust-card">
-    <img className="card-img-top pimg"  src='./coming-soon.jpg' alt='coming soon' />
+    <img className="card-img-top pimg"  src={Caffiends} alt='coffeeShop' />
     <div className="card-body">
-      <h5 className="card-title">Coming Soon</h5>
-      <p className="cust-card-text"> <a href='https://stamperm.github.io/walkItOut/'>Deployed Site</a></p>
-      <p className="cust-card-text"><small className="text-muted"><a href='https://github.com/StamperM/walkItOut' >GitHub Repo</a></small></p>
+      <h5 className="card-title">Caffiends Coffee Shop</h5>
+      <p className="cust-card-text"> <Link to ='https://github.com/devAsmi/caffiends'>Deployed Site</Link></p>
+      <p className="cust-card-text"><small className="text-muted"><Link to ='https://github.com/StamperM/walkItOut' >GitHub Repo</Link></small></p>
     </div>
 </div>
 
@@ -34,27 +46,27 @@ export default function  Portfolio(){
  <div className='container'> 
  <div className="row row-col">
   <div className="col card cust-card">
-    <img className="card-img-top pimg" src='./weatherApp.png' alt='weathersite' />
+    <img className="card-img-top pimg" src={Weather} alt='weathersite' />
     <div className="card-body">
       <h5 className="card-title">Weather App</h5>
-      <p className="cust-card-text"><a href='https://stamperm.github.io/WhatsTheWeather/' >Deployed Site</a></p>
-      <p className="cust-card-text"><small className="text-muted"> </small><a href='https://github.com/StamperM/WhatsTheWeather' >GitHub Repo</a></p>
+      <p className="cust-card-text"><Link to='https://stamperm.github.io/WhatsTheWeather/' >Deployed Site</Link></p>
+      <p className="cust-card-text"><small className="text-muted"> </small><Link to='https://github.com/StamperM/WhatsTheWeather' >GitHub Repo</Link></p>
     </div>
   </div>
   <div className="col card cust-card">
-    <img className="card-img-top pimg" src='./workoutsite.png' alt='workoutsite' />
+    <img className="card-img-top pimg" src={Workout}alt='workoutsite' />
     <div className="card-body">
       <h5 className="card-title">All Things Fit</h5>
-      <p className="cust-card-text"> <a href='https://homefitwellness.herokuapp.com/' >Deployed Site</a></p>
-      <p className="cust-card-text"><small className="text-muted"> <a href='https://github.com/StamperM/All-Things-Fit' >GitHub Repo</a></small></p>
+      <p className="cust-card-text"> <Link to='https://homefitwellness.herokuapp.com/' >Deployed Site</Link></p>
+      <p className="cust-card-text"><small className="text-muted"> <Link to='https://github.com/StamperM/All-Things-Fit' >GitHub Repo</Link></small></p>
     </div>
   </div>
   <div className="col card cust-card">
-    <img className="card-img-top pimg"  src='./OldPortfolio.png' alt='portfolio' />
+    <img className="card-img-top pimg"  src={Old} alt='portfolio' />
     <div className="card-body">
       <h5 className="card-title">Old Portfolio</h5>
-      <p className="cust-card-text"><a href='https://stamperm.github.io/gillen-portfolio-/' >Deployed Site</a></p>
-      <p className="cust-card-text"><small className="text-muted"><a href='https://github.com/StamperM/gillen-portfolio-' >GitHub Repo</a> </small></p>
+      <p className="cust-card-text"><Link to='https://stamperm.github.io/gillen-portfolio-/' >Deployed Site</Link></p>
+      <p className="cust-card-text"><small className="text-muted"><Link to='https://github.com/StamperM/gillen-portfolio-' >GitHub Repo</Link> </small></p>
   
     </div>
     </div>
